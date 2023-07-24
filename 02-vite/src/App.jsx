98f98258d60eb18399cab342d7d640c4b182164a
Julien Michon <mcn.julien@gmail.com>
2023-07-24 17:25:46 +0200

    React course

---
+++ 02-vite/src/App.jsx
@@ -0,0 +1,13 @@
+import Button from './button';
+
+function App() {
+  return ( 
+    <div>
+      <h1>Vite + React</h1>
+      <Button title="Valider" />
+      <Button title="Ajouter"/>
+    </div>
+   );
+}
+
+export default App;
