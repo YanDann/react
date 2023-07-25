768cce0a50b3375b7dd183230e9fa13c6f4615d6
Julien Michon <mcn.julien@gmail.com>
2023-07-25 17:29:52 +0200

    LifeCycle 1 done / LifeCycle 2 WIP

--- 02-vite/src/App.jsx
+++ 02-vite/src/App.jsx
@@ -7,6 +7,8 @@ import Text from './Text';
 import Button from './button';
 import Clock from './Clock';
 import { useState } from 'react';
+import LifeCycle1 from './Exercices/LifeCycle1';
+import LifeCycle2 from './Exercices/LifeCycle2';
 
 function App() {
   const [show, setShow] = useState(true);
@@ -14,22 +16,25 @@ function App() {
   return (
     <div>
       <h1>Vite + React</h1>
-      <Button title="Valider" />
+      {/* <Button title="Valider" />
       <Button title="Ajouter" />
 
       <Text />
-      <List />
+      <List /> */}
 
-      {show && <Clock timezone="Europe/Paris" />}
+      {/* {show && <Clock timezone="Europe/Paris" />}
       <button onClick={() => setShow(!show)}>
         Afficher / Cacher
       </button>
-      <Clock timezone="Asia/Tokyo" />
+      <Clock timezone="Asia/Tokyo" /> */}
 
       {/* <State1 />
       <State2 />
       <State3 />
       <StateChallenge /> */}
+
+      <LifeCycle1 />
+      <LifeCycle2 />
     </div>
   );
 }
